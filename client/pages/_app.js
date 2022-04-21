284c596ffddfc863c048698a7d046aa3e5cc7f13
Erkut <erkutos25@gmail.com>
2022-04-21 16:23:50 +0300

    feat: add basic top NavBar and implement Register & Login pages

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,8 +1,16 @@
+import TopNav from '../components/TopNav'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'antd/dist/antd.css'
+import '../public/css/styles.css'
+
 
 const App = ({Component, pageProps}) => {
-    return <Component {...pageProps}/>
+    return (
+    <>
+        <TopNav/>
+        <Component {...pageProps}/>
+    </>
+    )
 }
 
 export default App
