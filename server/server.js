ba71dae87ee39236c35ba95c3926812236b4e710
Erkut <erkutos25@gmail.com>
2022-04-22 14:23:39 +0300

    feat: set server, middleware, route and port so that server can start listening

--- server/server.js
+++ server/server.js
@@ -1,1 +1,28 @@
-console.log("server setup")
+import express from 'express'
+import cors from 'cors'
+const morgan = require('morgan')
+require('dotenv').config()
+
+//server
+const app = express()
+
+//middleware
+app.use(cors())
+app.use(express.json())
+app.use(morgan("dev"))
+app.use((req, res, next) => {
+    console.log("middleware")
+    next()
+})
+
+//route
+app.get('/', (req, res) => {
+    res.send('you hit the server endpoint')
+})
+
+//port
+const PORT = process.env.PORT || 8000
+
+app.listen(PORT, () => {console.log(`Server is running on port ${PORT}`)})
+
+
