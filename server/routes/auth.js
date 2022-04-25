fff33c62cfceebedcee98cb35bbfb8d488827abc
Erkut <erkutos25@gmail.com>
2022-04-25 20:57:13 +0300

    feat: handle mongoose connectionaxios request when register submit occurs

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,8 @@
 import express from 'express'
+import { register } from '../controllers/auth'
 
 const router = express.Router()
 
-router.get('/register', (req, res) => {
-    res.send('register user')
-})
+router.post('/register', register)
 
 module.exports = router
--- server/server.js
+++ server/server.js
@@ -1,11 +1,21 @@
 import express from 'express'
 import cors from 'cors'
-import {readdirSync} from 'fs'
+import { readdirSync } from 'fs'
+import mongoose from 'mongoose'
 const morgan = require('morgan')
 require('dotenv').config()
 
 //server
 const app = express()
+
+//database
+mongoose
+  .connect(process.env.DATABASE, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log("DB Error => ", err));
 
 //middleware
 app.use(cors())
