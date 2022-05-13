40b463187b71a8c10419662df08853744582facb
Erkut <erkutos25@gmail.com>
2022-05-13 12:12:45 +0300

    feat: restructure the login page and create global state with context/reducer

--- client/pages/login.js
+++ client/pages/login.js
@@ -1,10 +1,76 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
+import axios from 'axios'
+import { toast } from 'react-toastify'
+import { SyncOutlined } from '@ant-design/icons'
+import Link  from 'next/link'
+import {Context} from '../context'
 
 const Login = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [loading, setLoading] = useState(false)
+
+  const { state, dispatch } = useContext(Context)
+  console.log("STATE", state)
+
+  const handleSubmit = async (e) => {
+      e.preventDefault()
+      try {
+          setLoading(true)
+        const { data } = await axios.post(`/api/login`, {
+            email,
+            password
+        })
+        console.log("LOGIN RESPONSE", data)
+        dispatch({
+          type: "LOGIN",
+          payload: data
+        })
+        //toast.success('Successfully registered')
+        setLoading(false)
+      } catch (error) {
+          toast.error(error.response.data)
+          setLoading(false)
+      }
+      
+  } 
+
   return (
     <>
         <h1 className="jumbotron text-center bg-primary square">Login</h1>
-        <p>Login Page</p>
+        <div className="container col-md-4 offset-md-4 pb-5">
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type="email" 
+                    className='form-control mb-4 p-4' 
+                    value={email} 
+                    onChange={e => setEmail(e.target.value)}
+                    placeholder="Enter Email"
+                    required
+                />
+                <input 
+                    type="password" 
+                    className='form-control mb-4 p-4' 
+                    value={password} 
+                    onChange={e => setPassword(e.target.value)}
+                    placeholder="Enter Password"
+                    required
+                />
+                <button 
+                    type="submit" 
+                    className="btn col-12 btn-primary"
+                    disabled={!email || !password || loading}
+                >
+                    {loading ? <SyncOutlined spin/> : "Submit"}
+                </button>
+            </form>
+            <p className="text-center p-3">
+                Not Yet Registered?{" "}
+                <Link href='/register'>
+                    <a>Register</a>
+                </Link>
+            </p>
+        </div>
     </>
   )
 }
--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,6 @@
 import User from '../models/user'
 import { hashPassword, comparePassword } from '../utils/auth'
+import jwt from 'jsonwebtoken'
 
 export const register = async (req, res) => {
     try {
@@ -27,5 +28,31 @@ export const register = async (req, res) => {
     } catch (error) {
         console.log(error)
         return res.status(400).send('Error...')
+    }
+}
+
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body
+        const user = await User.findOne({email: email}).exec()
+
+        if(!user){
+            return res.status(400).send("No user found")
+        }
+
+        const match = await comparePassword(password, user.password)
+        const token = jwt.sign({_id: User._id}, process.env.JWT_SECRET, {expiresIn: '7d'})
+
+        user.password = undefined
+        res.cookie('token', token, {
+            httpOnly: true
+            //secure: true
+        })
+        res.json(user)
+    } catch (error) {
+        if(error){
+            console.log(error)
+            return res.status(400).send("Error! Please try again")
+        }
     }
 }
